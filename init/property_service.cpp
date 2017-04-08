ea7f67a1636a86b9c655d50924eed81f6591a134
Vivekachooz <vivekachooz@gmail.com>
2017-04-09 01:29:27 +0200

    init: Squashed import of snet spoofing
    init: Weaken property override security for the init extension
    
    Sometimes we need to override ro.* properties by using our vendor init
    extension.
    
    Previously there was a security check which was blocking that.
    To resolve the issue, we need to weaken the security check during the
    execution of our vendor init extension.
    
    This is safe because the vendor init extension gets executed as part of init
    construction and it is considered a trusted system component.
    
    Change-Id: I6095bbf92267fcb78ab53f37b5d7b443239ce80b
    
    init: workaround SafetyNet check
    
    Doing this in the userspace allows more properties to be spoofed
    and eliminate the needs for a hack in the kernel.
    
    Change-Id: I6ad755c085491c958c8a7d75db7df2c8e5481a55
    
    init: add vendor.*  keys to spoof safetynet
    
    aswinas@pixysos: add some more props from magisk hide to userspace hack by arter97
    
    Change-Id: Ib6ad1df4582a2f8333b0cbf650e79e947f6576d0
    
    init: Weaken property override security only when spoofing safetynet
    
    Change-Id: I1664b3b3fa0fc87bda683d53a56645f6d974ce01
    
    init: Only set safetynet props if not eng build
    
    Change-Id: I5c675e3391cc48a95ab9186047e9e9effca95406
    
    core: Add more props for snet spoofing
    
    Also reformat code
    
    Change-Id: I4c0bcb61fea5a7b051c3a770d34a3a09f17db1c4
    
    init: Check for fastbootd before spoofing safetynet props
    
    The real prop values must be retained in recovery/fastbootd in
    order for fastbootd to allow/deny flashing correctly based on the
    bootloader lock state. This is accomplished by checking androidboot keys
    in the kernel cmdline and bootconfig (necessary on Pixel 6), and not
    spoofing anything if the boot isn't a normal full-blown Android boot.
    
    @jhenrique09 - Adapt to PE
    
    Change-Id: I8795b16a90eea4e5a03f64a7a56478f01144256b
    
    init: Spoof more props
    
    Change-Id: Ic0ddbd6a0dd40c877248f7864082eddab2b32366
    
    init: Use `IsRecoveryMode()` for normal boot checks
    
    Checking androidboot.mode properties will never work on devices where this
    property is always absent, primarily non-Pixel devices.
    
    Use existing IsRecoveryMode() check instead which is ugly, but works for this
    very purpose.
    
    Change-Id: Idc79fb2bf45f0416b242a1e1aa12bdb07bcf56b9
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -132,6 +132,8 @@ struct PropertyAuditData {
     const char* name;
 };
 
+static bool weaken_prop_override_security = false;
+
 static int PropertyAuditCallback(void* data, security_class_t /*cls*/, char* buf, size_t len) {
     auto* d = reinterpret_cast<PropertyAuditData*>(data);
 
@@ -406,8 +408,8 @@ static std::optional<uint32_t> PropertySet(const std::string& name, const std::s
     } else {
         prop_info* pi = (prop_info*)__system_property_find(name.c_str());
         if (pi != nullptr) {
-            // ro.* properties are actually "write-once".
-            if (StartsWith(name, "ro.")) {
+            // ro.* properties are actually "write-once", unless the system decides to
+            if (StartsWith(name, "ro.") && !weaken_prop_override_security) {
                 *error = "Read-only property was already set";
                 return {PROP_ERROR_READ_ONLY_PROPERTY};
             }
@@ -873,6 +875,98 @@ static void load_override_properties() {
     }
 }
 
+static const char *snet_prop_key[] = {
+    "ro.boot.vbmeta.device_state",
+    "ro.boot.verifiedbootstate",
+    "ro.boot.flash.locked",
+    "ro.boot.selinux",
+    "ro.boot.veritymode",
+    "ro.boot.warranty_bit",
+    "ro.warranty_bit",
+    "ro.debuggable",
+    "ro.secure",
+    "ro.bootimage.build.type",
+    "ro.build.type",
+    "ro.system.build.type",
+    "ro.system_ext.build.type",
+    "ro.vendor.build.type",
+    "ro.vendor_dlkm.build.type",
+    "ro.product.build.type",
+    "ro.odm.build.type",
+    "ro.build.keys",
+    "ro.build.tags",
+    "ro.system.build.tags",
+    "ro.vendor.boot.warranty_bit",
+    "ro.vendor.warranty_bit",
+    "vendor.boot.vbmeta.device_state",
+    "vendor.boot.verifiedbootstate",
+    NULL
+};
+
+static const char *snet_prop_value[] = {
+    "locked", // ro.boot.vbmeta.device_state
+    "green", // ro.boot.verifiedbootstate
+    "1", // ro.boot.flash.locked
+    "enforcing", // ro.boot.selinux
+    "enforcing", // ro.boot.veritymode
+    "0", // ro.boot.warranty_bit
+    "0", // ro.warranty_bit
+    "0", // ro.debuggable
+    "1", // ro.secure
+    "user", // ro.bootimage.build.type
+    "user", // ro.build.type
+    "user", // ro.system.build.type
+    "user", // ro.system_ext.build.type
+    "user", // ro.vendor.build.type
+    "user", // ro.vendor_dlkm.build.type
+    "user", // ro.product.build.type
+    "user", // ro.odm.build.type
+    "release-keys", // ro.build.keys
+    "release-keys", // ro.build.tags
+    "release-keys", // ro.system.build.tags
+    "0", // ro.vendor.boot.warranty_bit
+    "0", // ro.vendor.warranty_bit
+    "locked", // vendor.boot.vbmeta.device_state
+    "green", // vendor.boot.verifiedbootstate
+    NULL
+};
+
+static void workaround_snet_properties() {
+    std::string build_type = android::base::GetProperty("ro.build.type", "");
+
+    // Bail out if this is recovery, fastbootd, or anything other than a normal boot.
+    // fastbootd, in particular, needs the real values so it can allow flashing on
+    // unlocked bootloaders.
+    if (IsRecoveryMode()) {
+        return;
+    }
+
+    // Exit if eng build
+    if (build_type == "eng") {
+        return;
+    }
+
+    // Weaken property override security to set safetynet props
+    weaken_prop_override_security = true;
+
+    std::string error;
+
+    // Hide all sensitive props 
+    LOG(INFO) << "snet: Hiding sensitive props";
+    for (int i = 0; snet_prop_key[i]; ++i) {
+        PropertySetNoSocket(snet_prop_key[i], snet_prop_value[i], &error);
+    }
+
+    // Extra pops
+    std::string build_flavor_key = "ro.build.flavor";
+    std::string build_flavor_value = android::base::GetProperty(build_flavor_key, "");
+    build_flavor_value = android::base::StringReplace(build_flavor_value, "userdebug", "user", false);
+    PropertySetNoSocket(build_flavor_key, build_flavor_value, &error);
+
+    // Restore the normal property override security after safetynet props have been set
+    weaken_prop_override_security = false;
+}
+
 // If the ro.product.[brand|device|manufacturer|model|name] properties have not been explicitly
 // set, derive them from ro.product.${partition}.* properties
 static void property_initialize_ro_product_props() {
@@ -1253,6 +1347,9 @@ void PropertyLoadBootDefaults() {
     property_initialize_ro_vendor_api_level();
 
     update_sys_usb_config();
+
+    // Workaround SafetyNet
+    workaround_snet_properties();
 }
 
 void PropertyLoadDerivedDefaults() {
